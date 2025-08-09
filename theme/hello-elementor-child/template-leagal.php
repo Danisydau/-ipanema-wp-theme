16de3bd56612b9f5bd6f4e54b4dcc5e5e1fda0cd
Danisydau <Danisydau@users.noreply.github.com>
2025-08-09 00:47:59 +0000

    style: phpcbf auto fixes [skip ci]

--- theme/hello-elementor-child/template-leagal.php
+++ theme/hello-elementor-child/template-leagal.php
@@ -3,19 +3,21 @@
  * Template Name: Legal Page
  * Template Post Type: page
  */
-if ( ! defined('ABSPATH') ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 get_header(); ?>
 
 <main class="site-main">
-  <div class="container mx-auto px-6 py-12">
-    <?php
-      // Start the loop to pull in this page’s content
-      while ( have_posts() ) : the_post();
-        the_content();
-      endwhile;
-    ?>
-  </div>
+	<div class="container mx-auto px-6 py-12">
+	<?php
+		// Start the loop to pull in this page’s content
+	while ( have_posts() ) :
+		the_post();
+		the_content();
+		endwhile;
+	?>
+	</div>
 </main>
 
 <?php get_footer(); ?>
-
